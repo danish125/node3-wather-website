561a7ba76807e55be1b80917edf709b66ea28544
danish <drehman073@gmail.com>
2021-07-08 14:53:24 +0500

    init commit

---
+++ src/app.js
@@ -0,0 +1,108 @@
+const request=require('request')
+const geocode=require('./utils/geocode')
+const forecast=require('./utils/forecast')
+const path= require('path') 
+const express=require('express')
+const hbs=require('hbs')
+const geoCode = require('./utils/geocode')
+
+const app=express()
+
+//define paths for express config
+const publicDirectoryPath=path.join(__dirname,'../public')
+const viewsPath=path.join(__dirname,'../templates/views')
+const partialPaths=path.join(__dirname,'../templates/partials')
+
+//setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialPaths)
+
+//setup static directory
+app.use(express.static(publicDirectoryPath))
+
+app.get('',(req , res)=>{
+    res.render('index',{
+        title: "Weather App",
+        name: "danish"
+    })
+})
+app.get('/about',(req, res)=>{ 
+    res.render('about', {
+        title: "About",
+        name: "Danish"
+    })    
+})
+app.get('/help',(req, res )=>{
+    res.render('help',{
+        message: "we are here to help",
+        title: 'Help',
+        name: 'Danish Rehman'
+    })
+})
+ 
+app.get('/weather',(req, res)=> {
+    console.log(req.query.address)
+    if(!req.query.address){
+        return res.send({
+            error: 'no adress searched'
+        })
+    }else{
+geocode(req.query.address,(error, {latitude, longitude, location}={})=>{
+    
+    if(error){
+        return res.send({
+            error
+        })
+    }
+   
+    forecast(latitude, longitude, (error, forecastData) => {
+        
+        if(error){
+            return res.send({
+                error
+            })
+        }
+        
+        return res.send({
+            location: location,
+            forecast: forecastData.weatherDescription,
+            temperature: forecastData.temperature,
+            address: req.query.address
+        })
+       
+})
+})
+
+}
+    
+   
+})
+app.get('/products',(req, res)=>{
+    if(!req.query.search){
+        return res.send({
+            error: 'you must provide a search term'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products:[]
+    })
+})
+app.get('/help/*',(req, res)=>{
+    res.render('error',{
+        title: '404',
+        errorText: 'help article not found',
+        name: 'danish'
+    })
+})
+app.get('*',(req, res)=>{
+    res.render('error',{
+        title: '404',
+        errorText: 'page not found',
+        name: 'danish'
+    })
+})
+app.listen(3000, ()=>{
+    console.log('server is runnin on 3000')
+})
